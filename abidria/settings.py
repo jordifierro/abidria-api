e64ebfe391b8e66cd0725f912a0cf944a52ee6d1
jordifierro <jordifierromulero@gmail.com>
2017-06-15 22:17:26 +0200

    Migrate database to postgresql

--- abidria/settings.py
+++ abidria/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'abidria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASS'],
+        'HOST': '',
+        'PORT': '',
     }
 }
 
